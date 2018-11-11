859817e91fc74db0e4b895f4c0e487811001a436
ninjawearingnike <37001558+ninjawearingnike@users.noreply.github.com>
2018-11-11 04:04:46 -0500

    Add files via upload

--- Tile.java
+++ Tile.java
@@ -2,24 +2,45 @@ import java.util.HashMap;
 
 public class Tile {
 
-private static HashMap<Integer, Integer> tileToID = new HashMap<Integer, Integer>();
+    private static HashMap<Integer, Point> tileToCoords = new HashMap<Integer, Point>();
 
-    public static void claimTile(int tile, int id) {
+    private static class Point {
+
+        private double x;
+        private double y;
+
+        public Point(double xCoor, double yCoor) {
+            this.x = xCoor;
+            this.y = yCoor;
+        }
+
+        public double getX(Point p) {
+            return p.x;
+        }
+
+        public double getY(Point p) {
+            return p.y;
+        }
+    }
+
+    public static void claimTile(int tile, double xCoor, double yCoor) {
         if (tile == 0)
         throw new IllegalArgumentException("Error!");
 
-        if (tileToID.containsKey(tile))
+        if (tileToCoords.containsKey(tile))
         throw new IllegalArgumentException("Tile is already marked");
 
-        else
-        tileToID.put(tile, id);
+        else {
+            Point p = new Point(xCoor, yCoor);
+            tileToCoords.put(tile, p);
+        }
     }
 
     public static void freeTile(int tile) {
-        if (tileToID.containsValue(id))
+        if (tileToCoords.containsKey(tile))
         throw new IllegalArgumentException("Tile is already free");
 
         else
-        tileToID.remove(tile);
+        tileToCoords.remove(tile);
     }
 }
